fff0cd374ebd0029199b2ac909e54aa763012d3e
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2023-11-22 22:03:19 +0100

    Added id to GetRecipeDto

--- Dtos/Recipes/GetRecipeDto.cs
+++ Dtos/Recipes/GetRecipeDto.cs
@@ -4,6 +4,7 @@ namespace HippoRecipeApi.Dtos.Recipes;
 
 public class GetRecipeDto
 {
+    public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public List<GetIngredientDto> Ingredients { get; set; }
