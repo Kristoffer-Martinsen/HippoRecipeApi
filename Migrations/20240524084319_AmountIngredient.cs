8dff42a988c8b6ce08f904b1c1cf933a688f887b
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2024-05-24 10:51:21 +0200

    Added amount to IngredientRecipe Junction table

---
+++ Migrations/20240524084319_AmountIngredient.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HippoRecipeApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AmountIngredient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Amount",
+                table: "IngredientRecipe",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "IngredientRecipe");
+        }
+    }
+}
